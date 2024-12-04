873eb14535f4ad4a2e74e5ac74afd3638c945e96
Giyoul <cla12093@gmail.com>
2024-12-04 17:39:07 +0900

    :bug: Fix data Axios post bug

--- src/components/local/DataTransform.js
+++ src/components/local/DataTransform.js
@@ -2,22 +2,25 @@ import axios from "axios";
 
 const transferData = async () => {
 	try {
-		// currentdata에서 데이터 가져오기
 		const response = await axios.get(
 			"https://6747ce2938c8741641d7b978.mockapi.io/api/currentdata"
 		);
 
-		// 데이터를 lastdata로 POST로 보내기
-		await axios.post(
-			"https://6747ce2938c8741641d7b978.mockapi.io/api/lastdata",
-			response.data
-		);
-
-		console.log("데이터가 성공적으로 옮겨졌습니다!");
+		if (response.data.length > 0) {
+			const dataToTransfer = response.data[0]; // 첫 번째 데이터를 선택
+			await axios.post(
+				"https://6747ce2938c8741641d7b978.mockapi.io/api/lastdata",
+				dataToTransfer
+			);
+			console.log("데이터가 성공적으로 옮겨졌습니다!");
+		} else {
+			console.log("currentdata에 데이터가 없습니다.");
+		}
 	} catch (err) {
 		console.error("데이터 옮기기 실패:", err);
 	}
 };
+
 
 // 이 파일을 import한 곳에서 실행하려면
 export const executeDataTransfer = () => {
